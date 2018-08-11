f3a0051e182767292c23182fef230510b1fbb6fa
Manohar Negi <manohar@hurreh.com>
2018-08-11 20:26:48 +0530

    refactored code to handle keyboard and keys

--- js/main.js
+++ js/main.js
@@ -1,129 +1,248 @@
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator-keys');
+var calc = {
+    operator: {
+        name: '',
+        arguments: ''
+    },
+    operand1: {
+        value: '',
+        touched: false
+    },
+    operand2: {
+        value: '',
+        touched: false
+    },
+    apiUrl: 'http://calctest.iesim.biz/',
+    operationUrl: '',
+
+    numericKeyPress: function(key){
+        if(this.operator.name === ''){
+            // first operand
+            if(!this.operand1.touched){
+                this.operand1.touched = true;
+                this.operand1.value = key;
+            }else{
+                this.operand1.value = this.operand1.value + key;
+            }
+            this.setDisplayValue(this.operand1.value);
+        }else{
+            this.setDisplayValue('0');
+            // second operand
+            if(!this.operand2.touched){
+                this.operand2.touched = true;
+                this.operand2.value = key;
+            }else{
+                this.operand2.value = this.operand2.value + key;
+            }
+            this.setDisplayValue(this.operand2.value);
+        }
+    },
+
+    operatorKeyPress: function(key, name, args){
+        // key.dataset.operator, key.dataset.args
+        this.operator.name = name;
+        this.operator.arguments = args;
+        if(key) {
+            this.highlightKey(key, true);
+        }
+        // also update operand value 1 = display value
+        this.operand1.value = display.textContent;
+    },
+
+    backspaceAction: function(){
+        var text = '';
+        if(this.operator.name === ''){    
+            if (!this.operand1.value.includes('.')) {
+                this.operand1.value = 
+                    this.operand1.value.substring(0, this.operand1.value.length - 1);
+                text = this.operand1.value;
+            }
+        }else{
+            if (!this.operand2.value.includes('.')) {
+                this.operand2.value = 
+                    this.operand2.value.substring(0, this.operand2.value.length - 1);
+                text = this.operand2.value;
+            }
+        }
+        this.setDisplayValue( (text=='') ? '0' : text);
+    },
+
+    equalsAction: function(){
+        this.calculate();
+        this.reset();
+        Array.from(keys.children).forEach(k => k.classList.remove('is-depressed'));
+    },
+
+    decimalAction: function(){
+        if(this.operator.name === ''){
+            if (!this.operand1.value.includes('.')) {
+                this.operand1.value = this.operand1.value + '.';
+                this.setDisplayValue(this.operand1.value);
+            }
+        }else{
+            if (!this.operand2.value.includes('.')) {
+                this.operand2.value = this.operand2.value + '.';
+                this.setDisplayValue(this.operand2.value);
+            }
+        }
+    },
+
+    clearAction: function(){
+        this.reset();
+        this.setDisplayValue('0');
+    },
+
+    handleButtonClick: function(key){
+        if(key && key.dataset && key.dataset.operator){
+            // check if operator is clicked
+            this.operatorKeyPress(key, key.dataset.operator, key.dataset.args);
+        }else if(key && key.dataset && key.dataset.action){
+            // check if action buttons clicked
+            switch(key.dataset.action){
+                case 'decimal':
+                    this.decimalAction();
+                    break;
+                case 'clear':
+                    this.clearAction();
+                    break;
+                case 'calculate':
+                    this.equalsAction();
+                    break;
+                default: break;
+            }
+        }else{
+            // numeric buttons clicked
+            this.numericKeyPress(key.textContent);
+        }
+    },
+
+    handleKeyPress: function(keyCode, key){
+        switch(keyCode){
+            case 13:
+            case 71:
+                // enter and =
+                this.equalsAction();
+                break;
+            case 27:
+                // esc
+                this.reset();
+                this.setDisplayValue('0');
+                break;
+            case 190:
+                // . decimal
+                this.decimalAction();
+                break;
+            case 8:
+                // backspace
+                this.backspaceAction();
+                break;
+            case 61:
+                // add
+                this.operatorKeyPress(keyAdd, 'add', '2');
+                break;
+            case 173:
+                // subtract
+                this.operatorKeyPress(keySubtract, 'subtract', '2');
+                break;
+            case 56:
+                // multiply
+                if(key && key== '*'){
+                    this.operatorKeyPress(keyMultiply, 'multiply', '2');
+                }else{
+                    this.numericKeyPress(String.fromCharCode(keyCode));
+                }
+                break;
+            case 191:
+                // divide
+                this.operatorKeyPress(keyDivide, 'divide', 2);
+                break;
+            case 54:
+                // power
+                if(key && key== '^'){
+                    this.operatorKeyPress(keyPower, 'power', 2);
+                }else{
+                    this.numericKeyPress(String.fromCharCode(keyCode));
+                }
+                break;
+            default:
+                if (keyCode >= 48 && keyCode <= 57) {
+                    this.numericKeyPress(String.fromCharCode(keyCode));
+                }
+                break;
+        }
+    },
+
+    reset: function(){
+        this.operator.name= '';
+        this.operator.arguments= ''; 
+        this.operand1.value= '';
+        this.operand1.touched= false;
+        this.operand2.value= '';
+        this.operand2.touched= false;
+        this.operationUrl ='';
+    },
+
+    setDisplayValue: function(value){
+        if(display){
+           display.textContent = value;
+        }
+    },
+
+    highlightKey: function(key, doHighlight){
+        Array.from(keys).forEach(k => k.classList.remove('is-depressed'));
+        if(doHighlight){
+            key.classList.add('is-depressed');
+        }
+    },
+    
+    calculate: function(){
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                    var data = JSON.parse(xmlhttp.responseText);
+                    display.textContent = data.result;
+                }
+                else {
+                    display.textContent = 'Error';
+                }
+            }
+        };
+        switch(this.operator.arguments){
+            case '2':
+                this.operationUrl = this.apiUrl + this.operator.name + '?op1=' + this.operand1.value + '&op2=' + this.operand2.value;
+                break;
+            case '1':
+                this.operationUrl = this.apiUrl + this.operator.name + '?op1=' + this.operand2.value;
+                break;
+            case '0':
+                this.operationUrl = this.apiUrl + this.operator.name;
+                break;
+            default:
+                //do nothing
+                break;
+        }
+        if(this.operationUrl !== this.apiUrl){
+            xmlhttp.open("GET", this.operationUrl, true);
+            xmlhttp.send();
+        }
+    }
+};
+
+const keys = document.querySelector('.calculator-keys');
 const display = document.querySelector('.calculator-display');
-var operator = '';
-var operand1 = '';
-var operand2 = '';
-var args = 0;
+/* operator keys */
+const keyAdd = document.querySelector('.key-add');
+const keySubtract = document.querySelector('.key-subtract');
+const keyMultiply = document.querySelector('.key-multiply');
+const keyDivide = document.querySelector('.key-divide');
+const keyPower = document.querySelector('.key-power');
 
-function resetValues(){
-    operator = '';
-    operand1 = '';
-    operand2 = '';
-    args = 0;
-}
-const calculate = (operator, operand1, operand2) => {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-            if (xmlhttp.status == 200) {
-                var data = JSON.parse(xmlhttp.responseText);
-                display.textContent = data.result;
-                resetValues();
-                operand1 = data.result;
-            }
-            else {
-                display.textContent = 'Error';
-            }
-        }
-    };
-
-    var endPoint = 'http://calctest.iesim.biz/';
-    endPoint = endPoint + operator;
-
-    if (operand1 && operand1 != null) {
-        endPoint = endPoint + '?op1=' + operand1;
-    }
-    if (operand2) {
-        endPoint = endPoint + '&op2=' + operand2;
-    }
-    xmlhttp.open("GET", endPoint, true);
-    xmlhttp.send();
-}
-
-keys.addEventListener('click', e => {
+keys.addEventListener('click', function(e){
     if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
-
-        if (!action) {
-            if (displayedNum === '0') {
-                display.textContent = keyContent;
-            } else {
-                display.textContent = displayedNum + keyContent;
-            }
-        } else {
-            if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide' || action === 'power') {
-                // Remove .is-depressed class from all keys
-                Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-                key.classList.add('is-depressed');
-                operand1 = displayedNum
-                display.textContent = '0'
-                operator = action
-                args = 2
-            } else if (action === 'square_root' || action === 'log10' || action === 'ln') {
-                console.log(action);
-                Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-                key.classList.add('is-depressed');
-                resetValues();
-                display.textContent = '0'
-                operand1 = '0'
-                operator = action
-                args = 1
-            } else if (action === 'pi' || action === 'e') {
-                calculate(action);
-                key.classList.remove('is-depressed');
-            } else if (action === 'decimal') {
-                if (!display.textContent.includes('.')) {
-                    display.textContent = displayedNum + '.'
-                }
-            } else if (action === 'clear') {
-                display.textContent = '0';
-                resetValues();
-            } else if (action === 'calculate') {
-                if(args == 1){
-                    calculate(operator, display.textContent);
-                }else if(args == 2){
-                    calculate(operator, operand1, display.textContent)
-                }
-                Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-            }
-        }
+        var key = e.target;
+        calc.handleButtonClick(key);
     }
 });
-
-document.body.addEventListener('keydown', e => {
-    var keyCode = e.keyCode;
-    if (keyCode == 13 || keyCode == 71) {
-        // enter
-        if(args == 1){
-            calculate(operator, display.textContent);
-        }else if(args == 2){
-            calculate(operator, operand1, display.textContent)
-        }
-        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-    } else {
-        const displayedNum = display.textContent;
-        console.log(keyCode);
-
-        if (keyCode >= 48 && keyCode <= 57) {
-            // 0 - 9
-            if (displayedNum === '0') {
-                display.textContent = String.fromCharCode(keyCode);
-            } else {
-                display.textContent = displayedNum + String.fromCharCode(keyCode);
-            }
-        }
-
-        if (keyCode == 27) {
-            // esc
-            display.textContent = '0';
-            resetValues();
-        }
-
-       
-
-    }
+document.body.addEventListener('keydown', function(e){
+    calc.handleKeyPress(e.keyCode, e.key);
 });
